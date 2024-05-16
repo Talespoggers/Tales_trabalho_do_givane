9dfa526897f0e90dec0f5972a3bbb21b74e6e2d2
Talespogger <talesramos291@gmail.com>
2024-05-15 22:20:53 -0300

    djfoçplsjh~gf odnh

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './estilo.css';
-import biscoito from './assets/biscoito.png';
+import bis_coito from './assets/biscoito.png';      
 
 class App extends Component{
   
@@ -11,13 +11,7 @@ class App extends Component{
     };
     this.quebrarBiscoito = this.quebrarBiscoito.bind(this);
     
-    this.frases = ['Siga os bons e aprenda com eles',
-    'O bom-senso vale mais do que muito conhecimento', 
-    'o riso é a menor distancia entre duas pessoas', 
-    'deixe de lado as preocupaçoes e seja feliz', 
-    'realize o obivio, pense no improvavel e conquiste o impossivel', 
-    'Acredite em milagres, mais nao dependa deles', 
-    'a maior barreira para o sucesso é o medo do fracasso'];
+    this.frases = ['Siga os bons e aprenda com os ruims','O bom-senso vale menos do que muito conhecimento', 'o riso é a menor distancia entre duas pessoas e um trem', 'deixe de lado as preocupaçoes e seja deprecivo', 'realize o obvio, pense no improvavel e conquiste o possivel', 'Acredite em milagres, mais dependa deles', 'a maior barreira para o sucesso é o medo do GUSTAVO'];
   }
   quebrarBiscoito(){
     let state = this.state;
@@ -25,4 +19,27 @@ class App extends Component{
     state.TextoFrase = this.frases[numeroAleatorio];
     this.setState(state);
   }
+
+  render(){
+return(
+  <div className='container'>
+    <img src={bis_coito} className="img" alt='Imagem do bis_coito'/>
+    <Botao acao={this.quebrarBiscoito} nome={"Abrir Biscoito"}/>
+    <h3 className='textoFrase'>{this.state.TextoFrase}</h3>
+  </div>
+);
+
+  }
 }
+
+class Botao extends Component{
+  render(){
+    return(
+      <div>
+        <button onClick={this.props.acao}>{this.props.nome}</button>
+      </div>
+    )
+  }
+}
+
+export default App;
